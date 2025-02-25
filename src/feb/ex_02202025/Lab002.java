d1f276a01d68cbd9447f721bdda97f321fb8e230
ramvenna369 <ramvenna369@gmail.com>
2025-02-26 01:09:50 +0530

    Second Push

--- src/feb/ex_02202025/Lab002.java
+++ src/feb/ex_02202025/Lab002.java
@@ -1,4 +1,10 @@
 package feb.ex_02202025;
 
 public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(2/2);
+        System.out.println(2+2);
+        System.out.println("hahaha");
+    }
 }
